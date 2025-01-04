806a42629f01337ad8ead4c5fe90f659061c8bd1
BugHunter-X <askzenai@gmail.com>
2025-01-04 03:56:04 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,8 @@
+proc safeProc {a b} {
+  if {$a == 0} {
+    return 0  ; Or return an error message: return "Division by zero"
+  }
+  return [expr {$b / $a}]
+}
+puts [safeProc 0 10]
+puts [safeProc 2 10]
